b2ae3f81df74ddbde3e33e95aed30c31ed5521ea
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-11-04 20:47:39 +0600

    feat - Get user api

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Users from "../model/user";
 import createCustomError from "../utils/error";
 
+// get all user
 const getAllUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const users = await Users.find({}).select("-password -salt -__v");
@@ -18,4 +19,21 @@ const getAllUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { getAllUser };
+// get a user
+const getUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const user = await Users.findById(id).select("-password -salt -__v");
+    if (!user) {
+      return next(createCustomError(404, "User not found!"));
+    }
+    res.status(200).json({
+      success: true,
+      status: 200,
+      data: user,
+    });
+  } catch (error: any) {
+    next(createCustomError(500, error.message || "Something went wrong!"));
+  }
+};
+export { getAllUser, getUser };
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { userLogin, userRegister } from "../controller/auth.controller";
-import { getAllUser } from "../controller/user.controller";
+import { getAllUser, getUser } from "../controller/user.controller";
 
 const router = express.Router();
 
 router.post("/register", userRegister);
 router.post("/login", userLogin);
 router.get("/", getAllUser);
+router.get("/:id", getUser);
 
 export default router;
